3b445f0c7127b05c5d8c79bd6793fee3b0acf7bc
Stephen <sheldon2521@gmail.com>
2019-03-27 09:12:20 -0400

    Added overflow check for bit manipulation solution

--- src/BitManipulation/ReverseDigits.java
+++ src/BitManipulation/ReverseDigits.java
@@ -29,6 +29,7 @@ public class ReverseDigits {
         int x = 123;
         int y = -123;
         int z = 120;
+        int bigNum = 1534236469;
 
         System.out.println("Brute force approach is..");
         System.out.println(reverseBrute(x));
@@ -39,7 +40,7 @@ public class ReverseDigits {
         System.out.println(reverse(x));
         System.out.println(reverse(y));
         System.out.println(reverse(z));
-
+        System.out.println(reverse(bigNum));
     }
 
     public static int reverseBrute(int x) {
@@ -67,12 +68,18 @@ public class ReverseDigits {
         return result;
     }
 
+    // Bit manipulation approach
     public static int reverse(int x) {
         int result = 0;
         while (x != 0) {
+            // Check if result is going to overflow if we multiply it by 10 (shift it over)
+            if ((result) > Integer.MAX_VALUE/10 || (result) < Integer.MIN_VALUE/10) {
+                return 0;
+            }
             result = result * 10 + (x % 10);
             x = x/10;
         }
+
         return result;
     }
 }
